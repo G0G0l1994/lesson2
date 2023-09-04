77e31b5b2be0be92f1d66feaccbe558b934bf166
Oleinikov Vasily <vasia94.94@mail.ru>
2023-09-04 19:24:27 +0300

    добавил задачу по циклу

---
+++ 4_while1_vas.py
@@ -0,0 +1,22 @@
+"""
+
+Домашнее задание №1
+
+Цикл while: hello_user
+
+* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+  пользователя “Как дела?”, пока он не ответит “Хорошо”
+   
+"""
+
+
+def hello_user():
+  phrase=''
+  while phrase!='Хорошо':
+    print("Как дела?")
+    phrase=input()
+
+
+    
+if __name__ == "__main__":
+    hello_user()
